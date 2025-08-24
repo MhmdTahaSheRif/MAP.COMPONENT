35172942957412a1b66aa970ad5da8c4ba54ecc0
MhmdTahaSheRif <96780591+MhmdTahaSheRif@users.noreply.github.com>
2025-08-24 11:36:56 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'EnterpriseMap';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  @ViewChild('emc', { static: true }) emcRef!: ElementRef;
+
+  triggerZoomToLayer(): void {
+    const event = new CustomEvent('zoomToLayer', {
+      detail: { MapLayerCode: 1 },
+    });
+    this.dispatchToMap(event);
+  }
+
+  triggerZoomToFeature(): void {
+    const event = new CustomEvent('zoomToFeature', {
+      detail: { MapLayerCode: 1, FeatureID: 19 },
+    });
+    this.dispatchToMap(event);
+  }
+
+  triggerIdentifyFeature(): void {
+    const event = new CustomEvent('identifyFeature', {
+      detail: { MapLayerCode: 9, FeatureID: 123456789012345 },
+    });
+    this.dispatchToMap(event);
+  }
+
+  private dispatchToMap(event: CustomEvent): void {
+    if (this.emcRef?.nativeElement) {
+      this.emcRef.nativeElement.dispatchEvent(event);
+    } else {
+      console.error('❌ Enterprise Map Control is not initialized.');
+    }
+  }
 }
